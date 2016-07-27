a45ef4e00053578350613184fb1e99c34b3066b4
Chris Helm <christopher.helm@gmail.com>
2016-07-27 17:51:31 +0200

    removing all refs to js or any front-end extension

--- setup.py
+++ setup.py
@@ -1,31 +1,7 @@
 from setuptools import setup
-from setuptools.command.develop import develop as _develop
-from setuptools.command.install import install as _install
-from notebook.nbextensions import install_nbextension
-from notebook.services.config import ConfigManager
-import os
-
-project_name = "jupyter_react"
-
-extension_dir = os.path.join(os.path.dirname(__file__), project_name, "static")
-
-class develop(_develop):
-    def run(self):
-        _develop.run(self)
-        install_nbextension(extension_dir, symlink=True,
-                            overwrite=True, user=False, destination=project_name)
-        cm = ConfigManager()
-        cm.update('notebook', {"load_extensions": {"jupyter_react/index": True } })
-
-class install(_install):
-    def run(self):
-        _install.run(self)
-        cm = ConfigManager()
-        cm.update('notebook', {"load_extensions": {"jupyter_react/index": True } })
 
 setup(name='jupyter-react',
-      cmdclass={'develop': develop, 'install': install},
-      version='0.0.2',
+      version='0.1.0',
       description='React component extension for Jupyter Notebooks',
       url='https://github.com/timbr-io/jupyter-react',
       author='Chris Helm',
@@ -33,11 +9,6 @@ setup(name='jupyter-react',
       license='MIT',
       packages=['jupyter_react'],
       zip_safe=False,
-      data_files=[
-        ('share/jupyter/nbextensions/ju', [
-            'jupyter_react/static/index.js'
-        ]),
-      ],
       install_requires=[
           "ipython",
           "traitlets"
